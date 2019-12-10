d4f1d41fd7a8ce61dae27ce42aefda96c8b6c419
Lx4444 <319970623@qq.com>
2019-12-11 02:42:35 +0800

    hhhhhh

--- load_data.py
+++ load_data.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-#test222
+#test222test
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
